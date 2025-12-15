4ed6b4031253f2ae20f3210411fcf5c154021704
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-12-15 18:33:36 +0600

    Create LIS AND LDS.cpp

---
+++ LIS AND LDS.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+#define out(x) cout << (x) << nl
+#define in cin >>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define f first
+#define s second
+#define pb push_back
+
+#define rs(v, n) (v).resize(n)
+#define as(v, sz, val) (v).assign(sz, val)
+
+#define pll pair<ll, ll>
+#define vll vector<ll>
+
+#define st set<ll>
+#define ml multiset<ll>
+
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define rrep(i, n) for(ll i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for(ll i = a; i <= b; i++)
+#define rrep1(i, b, a) for(ll i = b; i >= a; i--)
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+ll t;
+ll n;
+vll v;
+ll c1, c2;
+
+void solve(){
+    in n;
+    rs(v, n);
+    
+    rep(i, n) in v[i];
+    
+    c1 = 0;
+    c2 = 0;
+    
+    rep(i, n){
+        
+        if(v[i] >= 2) c2++;
+        else c1++;
+    }
+    
+    out(c2 + (c1 + 1)/2);
+}
+
+int main(){
+    FastIO(); 
+    
+    t = 1;
+    in t;
+    while(t--) solve();
+}
