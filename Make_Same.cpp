2a2cec4c8c4db76a314b2023ccd41b501ad6d28f
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2026-01-12 19:45:25 +0600

    Update Make_Same.cpp

--- Make_Same.cpp
+++ Make_Same.cpp
@@ -267,3 +267,122 @@ int main(){
     in T;
     while(T--) solve();
 }
+
+
+
+
+
+Let's see Another Approach : 👇
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+#define out(x) cout << (x) << nl
+#define in cin >>
+
+#define pinf LLONG_MAX
+#define ninf LLONG_MIN
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define f first
+#define sc second
+#define pb push_back
+#define ppb pop_back
+#define sz(x) (ll)x.size()
+
+#define rs(v, n) (v).resize(n)
+#define as(v, sz, val) (v).assign(sz, val)
+
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define vvll vector< vector<ll> >
+#define vpll vector<pll>
+#define S string
+
+#define St set<ll>
+#define Ms multiset<ll>
+#define Mp map<ll, ll>
+#define Ump unordered_map<ll, ll>
+
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define rrep(i, n) for(ll i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for(ll i = a; i <= b; i++)
+#define rrep1(i, b, a) for(ll i = b; i >= a; i--)
+
+bool isPrime(ll n){
+
+    if(n == 1) return 0;
+    else if(n == 2) return 1;
+    else if(n % 2 == 0) return 0;
+    
+    for(ll i = 3; i <= n/i; i += 2){
+        if(n % i == 0) return 0;
+    }
+    
+    return 1;
+}
+
+/*
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+*/
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+ll T;
+ll n;
+S a, b, c;
+vpll cnt;
+ll all0, all1;
+
+void solve(){
+    in n >> a >> b >> c;
+    as(cnt, 3, make_pair(0ll, 0ll) );
+    
+    rep(i, n){
+        (a[i] == '0') ? cnt[0].f++ : cnt[0].sc++;
+        (b[i] == '0') ? cnt[1].f++ : cnt[1].sc++;
+        (c[i] == '0') ? cnt[2].f++ : cnt[2].sc++;
+    }
+    
+    all0 = cnt[0].f + cnt[1].f + cnt[2].f;
+    all1 = cnt[0].sc + cnt[1].sc + cnt[2].sc;
+    
+    if(all0 % n || all1 % n) out(-1);
+    else{
+        if(all0 == n){
+            out(n - max({cnt[0].f, cnt[1].f, cnt[2].f}) );
+        }
+        else if(all1 == n){
+            out(n - max({cnt[0].sc, cnt[1].sc, cnt[2].sc}) );
+        }
+        else out(0);
+    }
+}
+
+int main(){
+    FastIO(); 
+    
+    T = 1;
+    in T;
+    while(T--) solve();
+}
